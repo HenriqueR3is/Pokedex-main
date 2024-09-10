576f6cef7f886431d98cd5345aa78c86e39d92a1
Henrique <henriquehkreis@gmail.com>
2024-09-10 02:48:52 -0300

    Estilizando com Bootstrap

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-// Rota principal que lista os Pokémons
 app.get("/", function (request: Request, response: Response) {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((res) => res.json())
     .then((data) => {
-      // Adiciona o ID ao resultado (extrai o ID da URL)
       const results = data.results.map((pokemon: any) => {
-        const id = pokemon.url.split("/").filter(Boolean).pop(); // Extrai o ID da URL
-        return { ...pokemon, id }; // Adiciona o ID ao objeto Pokémon
+        const id = pokemon.url.split("/").filter(Boolean).pop();
+        return { ...pokemon, id };
       });
       response.render("index", { results });
     });
 });
 
-// Nova rota para exibir detalhes de um Pokémon específico
 app.get("/pokemon/:id", function (request: Request, response: Response) {
   const { id } = request.params;
 
@@ -31,6 +28,27 @@ app.get("/pokemon/:id", function (request: Request, response: Response) {
     .catch((error) => {
       console.error("Erro ao buscar Pokémon:", error);
       response.status(500).send("Erro ao buscar Pokémon");
+    });
+});
+
+app.get("/pokemon", function (request: Request, response: Response) {
+  const { name } = request.query;
+  const query = typeof name === "string" ? name.toLowerCase() : "";
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Pokémon não encontrado");
+      }
+    })
+    .then((pokemon) => {
+      response.render("pokemon", { pokemon });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar Pokémon:", error);
+      response.status(404).send("Pokémon não encontrado");
     });
 });
 
